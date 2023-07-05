9ed51ab56962f7ecf97126aafae24ce6d5751ba5
Stephen <tama82045@gmail.com>
2023-07-05 14:56:21 +0800

    v0,00002

--- client.go
+++ client.go
@@ -2,15 +2,11 @@ package Coinbase_RESTapi
 
 import (
 	"bytes"
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 	"strconv"
-	"strings"
 	"time"
 
 	jsoniter "github.com/json-iterator/go"
@@ -38,18 +34,46 @@ func NewClient(key, secret, passphase string) *Client {
 	}
 }
 
-func (c *Client) newRequest(method, path string, body []byte) (*http.Request, error) {
-	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+func (c *Client) newRequest(method, path string, body []byte, sign bool) (*http.Request, error) {
+
 	u, _ := url.ParseRequestURI(Endpoint)
 	u.Path = u.Path + path
 	req, err := http.NewRequest(method, u.String(), bytes.NewReader(body))
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Add("CB-ACCESS-KEY", c.key)
-	req.Header.Add("CB-ACCESS-PASSPHRASE", c.passphase)
-	req.Header.Add("CB-ACCESS-TIMESTAMP", timestamp)
-	req.Header.Add("CB-ACCESS-SIGN", c.sign(timestamp, method, path, body))
-	req.Header.Add("Content-Type", "application/json")
+	if sign {
+		timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+		req.Header.Add("CB-ACCESS-KEY", c.key)
+		req.Header.Add("CB-ACCESS-PASSPHRASE", c.passphase)
+		req.Header.Add("CB-ACCESS-TIMESTAMP", timestamp)
+		req.Header.Add("CB-ACCESS-SIGN", c.sign(timestamp, method, path, body))
+		req.Header.Add("Content-Type", "application/json")
+	}
 	return req, nil
+}
+
+func (c *Client) sendRequest(method, path string, body []byte, sign bool) (*http.Response, error) {
+	req, err := c.newRequest(method, path, body, sign)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("status code: %d", res.StatusCode)
+	}
+	return res, nil
+}
+
+func decode(res *http.Response, out interface{}) error {
+	defer res.Body.Close()
+	b, err := io.ReadAll(res.Body)
+	err = json.Unmarshal([]byte(b), &out)
+	if err == nil {
+		return nil
+	}
+	return err
 }
